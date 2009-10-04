f319290af57c15a83cf9a2e8af203e982adaca8a
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2009-10-04 15:29:02 +0000

    feature: amazon code including signing

--- includes/amazon.inc.php
+++ includes/amazon.inc.php
@@ -1,4 +1,34 @@
-<?
+<?php
+
+function call_amazon($method,$request) {
+
+	$sec = 'fU6jiqSr7I/FOvl/DlcOXWb4pbfpaxJU5q65O/mT';
+	$keyid = "00KSGTK690RP8B0NWBR2"; 
+
+	$service = 'AWSECommerceService';
+	$ts = gmdate("Y-m-d\TH:i:s\Z");
+	$base_str = $method.$ts;
+
+	$sg = base64_encode(hash_hmac('sha256', $base_str, $sec, true));
+
+	$client = new SoapClient("https://webservices.amazon.com/AWSECommerceService/AWSECommerceService.wsdl");
+	$hset = Array('AWSAccessKeyId' => $keyid, 'Timestamp' => $ts, 'Signature'=> $sg);
+	$h = Array();
+	foreach ($hset as $k => $v) {
+		array_push($h, new SoapHeader('http://security.amazonaws.com/doc/2007-01-01/', $k, $v));
+	}
+	$sr= $client->__setSoapHeaders($h);
+
+	$a = Array("Service" => 'AWSECommerceService',"AWSAccessKeyId" => $keyid,"Operation" => $method, "Request" => $request);
+	try {
+		$result = $client->__soapCall($method, array($a));
+	} catch (SoapFault $sf) {
+		print $sf->getMessage();
+		return null;
+	}
+	return($result);
+}
+
 function amazon_getAlbum($artists,$album,$asin)
 {
 	if((!count($artists))&&(!$asin)) {
@@ -10,36 +40,15 @@ function amazon_getAlbum($artists,$album,$asin)
 		$artists[$key] = strtolower($tmp);
 	}
 	
-	
 	$album = eregi_replace(" - CD ?[0-9]","",$album);
 
-	#$soapclient = new soapclient("http://soap-eu.amazon.com/schemas3/AmazonWebServices.wsdl?locale=uk");
-	$soapclient = new soapclient("http://ecs.amazonaws.com/AWSECommerceService/2008-04-07/UK/AWSECommerceService.wsdl");
-	
-	#print_r_html($soapclient->__getFunctions());
-	
-	
-	$params = array (
-		'Request'=>array(
-			'Keywords'	=> htmlspecialchars($album),
-			'SearchIndex'	=> 'Music',
-		),
-		'AWSAccessKeyId'	=> '00KSGTK690RP8B0NWBR2',
-	);
-
-	try {	
-		if($asin) {
-	                $result = $soapclient->ItemLookup(array('AWSAccessKeyId'=>'00KSGTK690RP8B0NWBR2','Request'=>array('IdType'=>'ASIN','ItemId'=>$asin)));
-        	}
-        	else {
-			# print_r_html($params);
-			$result = $soapclient->ItemSearch($params);
-		}
-	
-	} Catch (SoapFault $soapError) {
-		print_r($soapError);
-		return(false);	
+	if($asin) {
+		$result = $result = call_amazon('ItemLookup',array('IdType'=>'ASIN','ItemId'=>$asin));
+   	}
+    else {    	
+		$result = call_amazon('ItemSearch',array('Keywords'     => htmlspecialchars($album),'SearchIndex'       => 'Music'));
 	}
+	
 	
 	if(!isset($result->Items->Item)) return(false);
 
@@ -47,14 +56,16 @@ function amazon_getAlbum($artists,$album,$asin)
 	$details = array();
 	foreach($items as $item) {
 		if($item->ASIN == $asin) {
-                	$details = $item;
-                        break;
+        	$details = $item;
+            break;
 		}
 	}
 	if(!$details) {
 		foreach($items as $node) {
+			print "====<br>\n";
 			$item = array("Artist"=>array());
 			foreach($node->ItemAttributes as $at=>$val) {
+			if(!is_object($val)) print "$at=$val<br/>\n";
 				if(($at == "Artist")&&(!is_array($val))) {
 					$item[$at] = array($val); // might be one artist or many
 				}
@@ -64,7 +75,7 @@ function amazon_getAlbum($artists,$album,$asin)
 			}
 			if(eregi("^$album",$item['Title'])) {
 
-#				print "found album $album, trying to verify artist<br>\n";
+				print "found album $album, trying to verify artist<br>\n";
 
 				foreach($item['Artist'] as $artist) {
 					$artist = strtolower($artist);
@@ -73,8 +84,7 @@ function amazon_getAlbum($artists,$album,$asin)
 						$details = $item;
 						$albumObj = new album();
 						$albumObj->getByName($album);
-						$albumObj->amazon_asin = $node->ASIN;
-						$albumObj->set("amazon_asin");
+						$albumObj->setField("amazon_asin",$node->ASIN);
 						break;
 					}
 				}
