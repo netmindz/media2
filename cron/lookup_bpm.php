104e3b8d5d7a203faf012508322cbdd17b85f917
will <will@125943eb-5c6a-4675-a07f-e28e5e9861df>
2011-05-05 16:58:29 +0000

    update: new bpm code

--- cron/lookup_bpm.php
+++ cron/lookup_bpm.php
@@ -2,21 +2,51 @@
 ini_set("include_path",".:../classes:../includes:" . ini_get('include_path'));
 require("standard.inc.php");
 
+$wav = "/tmp/lookupbpm." . getmypid() . ".wav";
+
 $db = new database();
 
 $tracks = new track();
-$tracks->getList("where bpm=0");
+$tracks->getList("where bpm=0","order by rand()");
 while($tracks->getNext()) {
+	if($tracks->bpm > 0) {
+		print ".";
+		continue;
+	}
 	$source = new source();
 	if($source->getTrackList($tracks)) {
+		$result = array();
 		$source->getNext();
 		print $source->path . "\t";
-		$cmd =  "bpmcount ".escapeshellarg($source->path)." | tail -n 1 | awk '{ print $1}'";
+		#$cmd =  "bpmcount ".escapeshellarg($source->path)." | tail -n 1 | awk '{ print $1}'";
+		$cmd = "mplayer -endpos 180 -ao pcm:file=$wav ".escapeshellarg($source->path);
+		exec($cmd . " 2>&1", $result, $return);
+		if($return) {
+			print "Command line: $cmd\n";	
+			print_r($result);
+			continue;
+		}
+		$cmd =  "soundstretch $wav -bpm 2>&1 | tee /tmp/soundstretch.log | grep 'Detected BPM rate ' | awk '{ print $4}'";
 		$bpm = trim(`$cmd`);
-		print $bpm . "\n";
-		$track = $source->getTrack();
-		$track->setProperties(array('bpm'=>$bpm));
-		$track->update();
+		if($bpm > 0) {
+			print $bpm . "\n";
+			$track = $source->getTrack();
+			$track->setProperties(array('bpm'=>$bpm));
+			$track->update();
+			if(strtolower($source->type) == "mp3") {
+				exec("id3v2 --TBPM $bpm " .  escapeshellarg($source->path));
+			}
+			elseif(strtolower($source->type) == "ogg") {
+				exec("vorbiscomment -a -t \"BPM=$bpm\" " .  escapeshellarg($source->path));
+			}
+			else {
+				print "Can't update metadata for $source->type\n";
+			}
+		}
+		else {
+			print "FAILED\n";
+		}
+		@unlink($wav);
 	}
 }	
 ?>
